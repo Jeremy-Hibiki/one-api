dead28c83113b37962705f6c537082aa066bdf13
蒋正颀 <jiangzhengqi@fmsh.com.cn>
2024-12-13 12:25:55 +0800

    update

--- relay/adaptor/zhipu/constants.go
+++ relay/adaptor/zhipu/constants.go
@@ -1,7 +1,10 @@
 package zhipu
 
 var ModelList = []string{
-	"chatglm_turbo", "chatglm_pro", "chatglm_std", "chatglm_lite",
-	"glm-4", "glm-4v", "glm-3-turbo", "embedding-2",
-	"cogview-3",
+	"glm-4-plus", "glm-4-0520", "glm-4-airx", "glm-4-air", "glm-4-long", "glm-4-flashx", "glm-4-flash",
+	"glm-4", "glm-3-turbo",
+	"glm-4v-flash", "glm-4v-plus", "glm-4v",
+	"cogview-3-plus", "cogview-3", "cogviewx",
+	"codegeex-4",
+	"embedding-2", "embedding-3",
 }
